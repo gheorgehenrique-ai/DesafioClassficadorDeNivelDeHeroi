4a79575e7b7395c80ef690ad9ac63e5227c8d736
George Henrique <gheorgehenrique@gmail.com>
2026-01-18 08:58:10 -0300

    Correct hero level classification logic
    Fix variable names and add hero classification logic.

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
-# DesafioClassficadorDeNivelDeHeroi
-O Desafio do Filipão é um exercício prático proposto pela DIO , ministrado pelo professor Felipe Aguiar , com o objetivo de aplicar conceitos fundamentais de programação.
+//variáriavel prinipal
+let nomeHeroi ="Jesus Cristo";
+let xoHeroi=7500;
+let nivel;
+
+// Estrutura de decisão
+if(xpHeroi < 1000){ nivel = "Ferro";
+} else if (xpHeori>=1001 && xpHeroi <= 2000){ nivel = "Bronae";
+} else if (xpHeroi>=2001 && xpHeroi <= 5000){ nivel = "Prata";
+} else if (xpHeroi>=5001 && xpHeroi <= 7000){ nivel ="Ouro"; 
+} else if (xpHeroi>=7001 && xpHeroi <= 8000){nivel = "Platina";
+} else if (xpHeori>=8001 && xpHeroi <= 9000){ nivel="Ascendente"; 
+} else if (xpHeroi>=9001 && xpHeroi <= 10000){ nivel="Imortal";
+} else if(xpHeroi > 10000){nivel="Radiante";
+}
+// saída de Final
+console.log(`O Herói de nome ${nomeHeroi} está no nível de ${nivel}`);
